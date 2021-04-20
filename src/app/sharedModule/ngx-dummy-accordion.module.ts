074d1a2a337dc09fee0cb260f4c151be5fefce86
KvngPatel <kevangpatel@gmail.com>
2021-04-20 22:29:52 +0530

    installed ngxDummy Accordion Library

---
+++ src/app/sharedModule/ngx-dummy-accordion.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {  AccordionModule,Accordion,IAccordionStyling } from '../../../node_modules/@ngx-dummy/accordion-simple/bundles/ngx-dummy-accordion-simple.umd.js'
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  exports:[
+    
+  ]
+})
+export class NgxDummyAccordionModule { }
